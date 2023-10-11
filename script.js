fba6860ea35e722677766e9b248aa219335aa3bd
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2023-10-11 17:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+// Function to replace the text inside the p element with Accio ipsum text
+function replaceParagraphText() {
+    const paragraph = document.getElementById('accio-paragraph');
+    if (paragraph) {
+        paragraph.textContent = 'Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.';
+    }
+}
+
+// Call the function to replace the text
+replaceParagraphText();
